ea4fdb4dc79e043d34a87d0a8e66ee6bfbaf96f9
Jhabiz <Jhabiz.Nourmohammadi@gmail.com>
2022-02-12 10:19:11 -0500

    Added the event, task, note folders

--- calendar/src/app/app.module.ts
+++ calendar/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { MonthGridComponent } from './month-grid/month-grid.component';
 import { MonthTitleComponent } from './month-grid/month-title/month-title.component';
 import { DayComponent } from './month-grid/day/day.component';
+import { EventComponent } from './month-grid/day/event/event.component';
+import { TaskComponent } from './month-grid/day/task/task.component';
+import { NoteComponent } from './month-grid/day/note/note.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MonthGridComponent,
     MonthTitleComponent,
-    DayComponent
+    DayComponent,
+    EventComponent,
+    TaskComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule,
