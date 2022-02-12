d43be977e111b93ed79dbde9c28df0fffca3121f
Jhabiz <Jhabiz.Nourmohammadi@gmail.com>
2022-02-12 11:23:36 -0500

    Added an event, note, and task

--- calendar/src/app/models/note.ts
+++ calendar/src/app/models/note.ts
@@ -3,9 +3,9 @@ export class Note{
     updateDate: Date;
     text: string;
     title: string;
-    constructor(createDate: Date, updateDate: Date, text: string, title: string ){
-        this.createDate =createDate;
-        this.updateDate = updateDate;
+    constructor(createDate: Date = null, updateDate: Date = null, text: string, title: string ){
+        this.createDate =createDate || new Date();
+        this.updateDate = updateDate || new Date();
         this.text = text;
         this.title = title;
     }
--- calendar/src/app/services/event.service.ts
+++ calendar/src/app/services/event.service.ts
@@ -7,8 +7,21 @@ import { Event } from '../models/event';
 })
 export class EventService {
 
-  constructor() { }
-  event: Event[];
-  task: Task[];
-  note: Note[];
+  constructor() { 
+    this.events = [];
+    this.events.push( new Event(new Date("2022-02-14T11:00:00") ,
+    new Date("2022-02-14T13:00:00"),
+    "Birthday Party: Hannah"
+     ));
+    this.tasks = [];
+    this.tasks.push(new Task(new Date("2022-02-16T16:30:00"),
+     "SWE 632 HW1 Due"));
+    this.notes = [];
+    this.notes.push(new Note( new Date("2022-02-06T10:00:00"),
+    new Date("2022-02-08T 11:00:00"), 
+    "eggs, bread, butter", "grocery list"));
+  }
+  events: Event[];
+  tasks: Task[];
+  notes: Note[];
 }
