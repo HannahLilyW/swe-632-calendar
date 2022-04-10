b5ff611e9d11280d8655ec0eef42b1061ec8ad48
yuganksinghal <s.yugank@gmail.com>
2022-04-10 19:57:03 -0400

    update for mobile friendliness

--- calendar/src/app/month-grid/month-title/month-title.component.ts
+++ calendar/src/app/month-grid/month-title/month-title.component.ts
@@ -10,8 +10,9 @@ import { FormBuilder, FormGroup, Validators, ValidationErrors } from '@angular/f
 export class MonthTitleComponent implements OnInit {
   @Input() month: number;
   @Input() year: number;
-  @Output() next = new EventEmitter();
-  @Output() previous = new EventEmitter();
+  @Input() date: number;
+  @Output() nextMonth = new EventEmitter();
+  @Output() previousMonth = new EventEmitter();
   @Output() dateChange = new EventEmitter();
 
   monthNames: string[] = [
@@ -29,6 +30,8 @@ export class MonthTitleComponent implements OnInit {
     MonthNames.DEC
   ];
 
+  currentDate: string
+
   allowedYears: number[] = [];
 
   formGroup: FormGroup;
@@ -41,7 +44,10 @@ export class MonthTitleComponent implements OnInit {
     this.formGroup = this.formBuilder.group({
       month: [this.month, [Validators.required]],
       year: [this.year, [Validators.required]],
+      date: [this.date]
     });
+    
+    this.setDate();
   }
 
   populateAllowedYears = () => {
@@ -50,24 +56,63 @@ export class MonthTitleComponent implements OnInit {
     }
   }
 
-  monthYearChange = () => {
-    this.dateChange.emit(new Date(parseInt(this.formGroup.controls.year.value), parseInt(this.formGroup.controls.month.value)));
+  monthYearChange = (e) => {
+    if (e.target.name === "mobileDate") {
+      let split = e.target.value.split("-");
+      this.formGroup.controls.year.setValue(split[0]);
+      this.formGroup.controls.month.setValue(`${parseInt(split[1])-1}`);
+      this.formGroup.controls.date.setValue(split[2]);
+    }
+    this.dateChange.emit(new Date(parseInt(this.formGroup.controls.year.value), parseInt(this.formGroup.controls.month.value), parseInt(this.formGroup.controls.date.value)));
   }
 
-  goToNextMonth = () => {
-    this.next.emit();
+  goToNextMonth = (emit:boolean = true) => {
+    if (emit) this.nextMonth.emit();
     this.formGroup.controls.month.setValue((parseInt(this.formGroup.controls.month.value) + 1) % 12);
     if (parseInt(this.formGroup.controls.month.value) === 0) {
       this.formGroup.controls.year.setValue(parseInt(this.formGroup.controls.year.value) + 1);
     }
+    this.setDate();
   }
 
-  goToPreviousMonth = () => {
-    this.previous.emit();
+  goToPreviousMonth = (emit:boolean = true) => {
+    if (emit) this.previousMonth.emit();
     this.formGroup.controls.month.setValue((parseInt(this.formGroup.controls.month.value) + 11) % 12);
     if (parseInt(this.formGroup.controls.month.value) === 11) {
       this.formGroup.controls.year.setValue(parseInt(this.formGroup.controls.year.value) - 1);
     }
+    this.setDate();
   }
 
+  goToNextDay = () => {
+    let newDay = ((this.formGroup.controls.date.value + 1) % this.getLastDayOfMonth()) === 0 ? this.getLastDayOfMonth() : (this.formGroup.controls.date.value + 1) % this.getLastDayOfMonth();
+    if (newDay === 1) {
+      this.goToNextMonth(false);
+    }
+    this.formGroup.controls.date.setValue(newDay);
+    this.setDate();
+    this.dateChange.emit(new Date(parseInt(this.formGroup.controls.year.value), parseInt(this.formGroup.controls.month.value), parseInt(this.formGroup.controls.date.value)));
+  }
+
+  goToPreviousDay = () => {
+    let lastDayofPreviousMonth = this.getLastDayOfMonth(this.formGroup.controls.month.value)
+    let newDay = (this.formGroup.controls.date.value - 1) < 1 ? lastDayofPreviousMonth : this.formGroup.controls.date.value - 1;
+    if (newDay === lastDayofPreviousMonth && (this.formGroup.controls.date.value - 1) < 1) {
+      this.goToPreviousMonth(false);
+    }
+    this.formGroup.controls.date.setValue(newDay);
+    this.setDate();
+    this.dateChange.emit(new Date(parseInt(this.formGroup.controls.year.value), parseInt(this.formGroup.controls.month.value), parseInt(this.formGroup.controls.date.value)));
+  }
+
+  getLastDayOfMonth = (m=this.formGroup.controls.month.value + 1, y=this.formGroup.controls.year.value ) => {
+    let d = new Date(y, m, 0);
+    return d.getDate();
+  }
+
+  setDate() {
+    let month = parseInt(this.formGroup.controls.month.value) + 1 < 10 ? `0${parseInt(this.formGroup.controls.month.value) + 1}` : parseInt(this.formGroup.controls.month.value) + 1;
+    let date = parseInt(this.formGroup.controls.date.value) < 10 ? `0${this.formGroup.controls.date.value}` : this.formGroup.controls.date.value;
+    this.currentDate = `${this.formGroup.controls.year.value}-${month}-${date}`
+  }
 }
